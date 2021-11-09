c44826d56e53f02a34521e761fee652e8ad30d32
iHeloa <82479317+iHeloa@users.noreply.github.com>
2021-11-08 20:30:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+//https://teachablemachine.withgoogle.com/models/7vJNJ-8cq/
+Webcam.set({
+    width:360,
+    height:300,
+    image_format:'png',
+    png_quality:90 
+});
+camera=document.getElementById("camera")
+Webcam.attach("#camera")   
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image"src="'+data_uri+'"/>';
+    });
+}
+ console.log('ml5.version:', ml5.version);
+ classifier=ml5.imageClassifier('//https://teachablemachine.withgoogle.com/models/7vJNJ-8cq/model.json',modelLoaded);
+
+ function modelLoaded(){
+     console.log('Model Loaded!');
+ }
+
+ function speak(){
+     var synth=window.speechSynthesis;
+     speak_data_1="The first prediction is"+ prediction_1;
+     speak_data_2="The second prediction is"+ prediction_2;
+     var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+     synth.speak(utterThis);
+ }
+
+ function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+ }
+
+
+
+
+
+  function gotResult(error, results){
+      if (error) {
+          console.error(error);
+      } else {
+          console.log(results)
+          document.getElementById("result_handsign_name").innerHTML=results[0].label;
+          document.getElementById("result_handsign_name2").innerHTML=results[1].label;
+
+          prediction_1 = results[0].label;
+          prediction_2 = results[1].label;
+          speak();
+          if(results[0].label=="Thumbs up"){
+              document.getElementById("update_emoji").innerHTML="&#128406;";
+          }
+          if(results[0].label=="Thumbs down"){
+              document.getElementById("update_emoji").innerHTML="&#128077;";
+          }
+          if(results[0].label=="Live long and prosper"){
+              document.getElementById("update_emoji").innerHTML="&#128078;";
+          }
+
+          if(results[1].label=="Thumbs up"){
+            document.getElementById("update_emoji2").innerHTML="&#128406;";
+        }
+        if(results[0].label=="Thumbs down"){
+            document.getElementById("update_emoji2").innerHTML="&#128077;";
+        }
+        if(results[0].label=="Live long and prosper"){
+            document.getElementById("update_emoji2").innerHTML="&#128078;";
+        }
+      }
+  }
